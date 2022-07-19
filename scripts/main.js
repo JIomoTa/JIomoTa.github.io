c123c6e7ac9fcd0161c96b801080d16619bce522
JIomoTa <JIomoTa9@gmail.com>
2022-07-19 21:22:33 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,45 +12,23 @@ myImage.onclick = function() {
     }
 }
 
-var myButton = document.querySelector("button");
-var myHeading = document.querySelector("h1");
+// var myButton = document.querySelector("button");
+// var myHeading = document.querySelector("h1");
 
-function setUserName() {
-    var myName = prompt("Пожалуйста введите ваше имя");
-    if (myName === null){
-        myName = "";
-    }
-    localStorage.setItem("name",myName);
-    myHeading.textContent = "Добро Пожаловать " + myName;
-}
-if(!localStorage.getItem("name")){
-    setUserName();
-} else {
-    var storedName = localStorage.getItem("name");
-    myHeading.textContent = "Добро Пожаловать " + storedName;
-}
-myButton.onclick = function() {
-    setUserName();
-}
-
-/*const list = document.createElement('ul');
-const info = document.createElement('p');
-const html = document.querySelector('html');
-
-info.textContent = 'Это динамический список. Кликните в любем месте данной страницы чтобы создать новый пункт.';
-
-document.body.appendChild(info);
-document.body.appendChild(list);
-
-list.onclick = function() {
-  const listItem = document.createElement('li');
-  const listContent = prompt('Что вы хотите добавить в данный список?');
-  listItem.textContent = listContent;
-  list.appendChild(listItem);
-
-  listItem.onclick = function(e) {
-    e.stopPropagation();
-    const listContent = prompt('Введите содержимое списка.');
-    this.textContent = listContent;
-  }//На потом!!!
-}*/
+// function setUserName() {
+//     var myName = prompt("Пожалуйста введите ваше имя");
+//     if (myName === null){
+//         myName = "";
+//     }
+//     localStorage.setItem("name",myName);
+//     myHeading.textContent = "Добро Пожаловать " + myName;
+// }
+// if(!localStorage.getItem("name")){
+//     setUserName();
+// } else {
+//     var storedName = localStorage.getItem("name");
+//     myHeading.textContent = "Добро Пожаловать " + storedName;
+// }
+// myButton.onclick = function() {
+//     setUserName();
+// }
